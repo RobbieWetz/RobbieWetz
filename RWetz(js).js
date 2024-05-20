a5328b34929df30c90a244dbf7b6b06b5fc68a2a
RobbieWetz <166842356+RobbieWetz@users.noreply.github.com>
2024-05-20 00:55:12 -0400

    Update RWetz(js).js

--- RWetz(js).js
+++ RWetz(js).js
@@ -12,7 +12,8 @@ var stonehousemarker = L.marker([40.61833798043794, -74.10818864897371]).addTo(m
 // Add a popup to the marker
 stonehousemarker.bindPopup(`
     <b>The Stone House</b><br>Scenic and high quality restaurant within Clove Lakes Park.<br>
-    img src="https://RobbieWetz.github.io/RobbieWetz/Images/").openPopup();
+    <img src="https://RobbieWetz.github.io/RobbieWetz/Images/StoneHouse.jpg" alt="The Stone House" style="width: 100px; height: auto;">
+    `).openPopup();
 
 // Add a marker to the map for New York City
 var marker = L.marker([40.61797668668023, -74.1056101967246]).addTo(map);
